b5da1859ed01b67a9921922a205bf0b8432acbc6
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-06 08:30:14 +0800

    First commit

---
+++ src/DailyTaskManager.java
@@ -0,0 +1,14 @@
+public class DailyTaskManager {
+    String[] tasks = {"File a report to the Administrator",
+    "Polish HEV Suit",
+    "Write an Email to Dr. Vance",
+    "Tidy up my office desk",
+    "Attend a meeting",
+    "Cook a Casserole",
+    "Make marketing content",
+    "Play Counter Strike"};
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
