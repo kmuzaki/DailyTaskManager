e8539a4e7163877e9052965019d7b7a8466d501b
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-06 21:42:37 +0800

    init select array or LL menu

--- src/DailyTaskManager.java
+++ src/DailyTaskManager.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
 public class DailyTaskManager {
+
     String[] tasks = {"File a report to the Administrator",
     "Polish HEV Suit",
     "Write an Email to Dr. Vance",
-    "Tidy up my office desk",
+    "Tidy up office desk",
     "Attend a meeting",
     "Cook a Casserole",
-    "Make marketing content",
-    "Play Counter Strike"};
+    "Spend time with the wife and kids"};
+
+    public static void selectVer(){
+        while (true){
+            System.out.println("=============================");
+            System.out.println("Please select your selection");
+            System.out.println("1. Array");
+            System.out.println("2. Linked List");
+            System.out.println("=============================");
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hello and welcome to the program, User!");
     }
 }
