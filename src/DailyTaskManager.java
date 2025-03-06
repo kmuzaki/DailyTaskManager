2b03ce7d8a3827019567bce9764e4c67d25b5bfa
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-06 21:54:37 +0800

    functional startMenu

--- src/DailyTaskManager.java
+++ src/DailyTaskManager.java
@@ -11,17 +11,39 @@ public class DailyTaskManager {
     "Cook a Casserole",
     "Spend time with the wife and kids"};
 
+    
+
     public static void selectVer(){
+        // Initialise scanner for user input
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
         while (true){
             System.out.println("=============================");
             System.out.println("Please select your selection");
             System.out.println("1. Array");
             System.out.println("2. Linked List");
             System.out.println("=============================");
+
+            System.out.print("Type in selection> ");
+            input = scanner.next();
+
+            if (input.equals("1")){
+                //Block of array code
+
+            } else if (input.equals("2")){
+                //Block of Linked List code
+            
+            } else {
+                System.out.println("Invalid Input! Please check your input again.");
+                System.out.println("");
+            }
+
         }
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello and welcome to the program, User!");
+        selectVer();
     }
 }
