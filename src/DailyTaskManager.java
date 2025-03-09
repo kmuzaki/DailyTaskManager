fa5da3114810004ea4c995f8861357594d67b831
Kmuzaki <keihanmuzaki0480@gmail.com>
2025-03-09 14:23:40 +0800

    Fixed update task feature

--- src/DailyTaskManager.java
+++ src/DailyTaskManager.java
@@ -13,13 +13,60 @@ public class DailyTaskManager {
     
     // Initialise scanner object
     static Scanner scanner = new Scanner(System.in);
-    static int selection;
+    static int selectionInt;
+    static String selectionString;
 
     // Clearscreen function
     public static void clearScreen() {  
         System.out.print("\033[H\033[2J");  
         System.out.flush();  
-    }  
+    }
+
+    // Function to display all tasks within an array
+    public static void displayTasks(){
+        System.out.println("");
+        System.out.println("Here are your current tasks: ");
+        for (int i = 0; i < arrayTasks.length; i++) {
+            System.out.println((i + 1) + ". " + arrayTasks[i]);
+            }
+        // System.out.println("");
+    }
+
+    // Function to manipulate and update task
+    public static void updateTask(){
+        while (true) {
+            displayTasks();
+            System.out.println("");
+            System.out.println("Select task that you would like to update");
+    
+            try {
+                System.out.print("Type in selection or '0' if you'd like to go back> ");
+                selectionInt = scanner.nextInt();
+                scanner.nextLine(); // Consume leftover newline
+    
+                if (selectionInt == 0) {
+                    break;
+                } else if (selectionInt > 0 && selectionInt <= arrayTasks.length) {
+                    selectionInt--; // Adjust index for zero-based array
+                    System.out.println("Selected task: " + arrayTasks[selectionInt]);
+                    
+                    System.out.print("Type in new task for the selected task> ");
+                    selectionString = scanner.nextLine(); // Read string input correctly
+    
+                    arrayTasks[selectionInt] = selectionString; // Update task in array
+                    System.out.println("Task updated!");
+                    System.out.println("");
+                    break;
+                } else {
+                    System.out.println("Invalid selection! Please enter a valid task number.");
+                }
+    
+            } catch (Exception e) {
+                System.out.println("Invalid Input! Please check your input again.");
+                scanner.nextLine(); // Consume invalid input
+            }
+        }
+    }
 
     // Array version of task manager menu
     public static void arrayVerMenu(){
@@ -33,21 +80,18 @@ public class DailyTaskManager {
 
             try {
                 System.out.print("Type in selection> ");
-                selection = scanner.nextInt();
+                selectionInt = scanner.nextInt();
 
-                if (selection == 1) {
+                if (selectionInt == 1) {
                     // Display all tasks in the array via looping
+                    displayTasks();
                     System.out.println("");
-                    System.out.println("Here are your current tasks: ");
-                    for (int i = 0; i < arrayTasks.length; i++) {
-                        System.out.println((i + 1) + ". " + arrayTasks[i]);
-                    }
-                    System.out.println("");
-
-                } else if (selection == 2) {
+                    
+                } else if (selectionInt == 2) {
                     // Block of Linked List code
-                    System.out.println("You selected Linked List.");
-                } else if (selection == 3) {
+                    updateTask();
+
+                } else if (selectionInt == 3) {
                     // Block of Linked List code
                     System.out.println("See you next time!");
                     break;
@@ -73,14 +117,14 @@ public class DailyTaskManager {
 
             try {
                 System.out.print("Type in selection> ");
-                selection = scanner.nextInt();
+                selectionInt = scanner.nextInt();
 
-                if (selection == 1) {
+                if (selectionInt == 1) {
                     // Block of array code
                     clearScreen();
                     System.out.println("You selected Array.");
                     arrayVerMenu();
-                } else if (selection == 2) {
+                } else if (selectionInt == 2) {
                     // Block of Linked List code
                     System.out.println("You selected Linked List.");
                 } else {
@@ -104,4 +148,5 @@ public class DailyTaskManager {
 /* Code references
  w3Schools, Java Tutorial, http://www.w3schools.com/java/
  How to clear console using Java, https://stackoverflow.com/questions/2979383/how-to-clear-the-console-using-java
+ Java Program to print elements of an array, https://www.geeksforgeeks.org/java-program-to-print-the-elements-of-an-array/
  */
